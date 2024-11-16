d1f808479e84d54b8746aaeebd81c76d365f5cf3
hunihunor <hunihunor90@gmail.com>
2024-11-16 10:16:32 +0100

    adminTable progress

--- src/pages/Adminpage.jsx
+++ src/pages/Adminpage.jsx
@@ -1,39 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Table } from 'react-bootstrap'
+import { ApiContext } from '../context/ApiContext'
 
 function Adminpage() {
+  const {getAdat} = useContext(ApiContext)
   return (
     <div>
         <h1>
             Készlet infó
         </h1>
-        <Table striped>
+        <Table striped bordered hover>
       <thead>
         <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
+          <th>ID</th>
+          <th>Termék név</th>
+          <th>Ár</th>
+          <th>kategória</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
+        <getAdat className="map"></getAdat>
       </tbody>
     </Table>
     </div>
