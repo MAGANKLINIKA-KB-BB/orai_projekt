64c98519d1ac7b6355077e058440a73b01db329c
BelaBordacs <side1111@hotmail.com>
2024-11-16 14:44:54 +0100

    login - design

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import './Login.css';
 
 const Login = () => {
+  
   return (
-    <div>Login</div>
-  )
+    <div className='form'>
+      <h1 className='loginh1'>Please log in</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address:</Form.Label>
+          <Form.Control type="email" placeholder="email@email.com" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+    </Form>
+    <Button variant="primary" onClick={() =>{console.log("Bejelentkezés gomb megnyomva")}}>Log in</Button>
+    </div>
+
+  );
 }
 
 export default Login
