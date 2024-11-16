a171908bbcfc22e1a550e20dca541153e051f4c6
karsaibence <karesz333@hotmail.com>
2024-11-16 10:27:28 +0100

    menu 0.02

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ApiProvider } from './context/ApiContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <ApiProvider>
-    <App />
-  </ApiProvider>
-    
+    <ApiProvider>
+      <App />
+    </ApiProvider>
+
   </React.StrictMode>
 );
 
